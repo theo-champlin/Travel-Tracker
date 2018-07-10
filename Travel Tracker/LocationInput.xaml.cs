f926482a394a9ad7946bffe4aadae398eace90db
Theo J. Champlin <tchamplin@factset.com>
2018-07-09 23:39:13 -0700

    And the rest of the project follows.

---
+++ Travel Tracker/LocationInput.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Travel_Tracker
+{
+   public partial class LocationInput : Window
+   {
+      public static string Country = String.Empty;
+      public static string City = String.Empty;
+
+      public ObservableCollection<string> TypeaheadCountryList { get; set; }
+      public ObservableCollection<string> TypeaheadCityList { get; set; }
+
+      public string CountrySelectionText { get; set; }
+      public string CitySelectionText { get; set; }
+
+      private struct CityInfo
+      {
+         [JsonProperty("country")]
+         public string Country;
+
+         [JsonProperty("name")]
+         public string Name;
+      }
+
+      private List<CityInfo> AllCities;
+
+      public LocationInput()
+      {
+         InitializeComponent();
+
+         using (var cityListfile = File.OpenText(@"../../CityLookup.json"))
+         using (var jsonTextReader = new JsonTextReader(cityListfile))
+         {
+            AllCities = new JsonSerializer().Deserialize<IEnumerable<CityInfo>>(jsonTextReader).ToList();
+         }
+         TypeaheadCountryList = new ObservableCollection<string>(AllCities.Select(city => city.Country).Distinct());
+         TypeaheadCityList = new ObservableCollection<string> { };
+
+         CountrySelectionText = String.Empty;
+         CitySelectionText = String.Empty;
+      }
+
+      private void OnCountrySet(object sender, RoutedEventArgs e)
+      {
+         Country = CountrySelectionText;
+         AllCities.Where(city => city.Country == Country).ToList().ForEach(city => TypeaheadCityList.Add(city.Name));
+      }
+
+      private void SetLocationClick(object sender, RoutedEventArgs e)
+      {
+         Country = CountrySelectionText;
+         City = CitySelectionText;
+         Close();
+      }
+   }
+}
---
+++ Travel Tracker/TimerWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
+using System.Linq;
+using System.Globalization;
+using System.Windows.Media.Imaging;
+
+namespace Travel_Tracker
+{
+   /// <summary>
+   /// Interaction logic for TimerWindow.xaml
+   /// </summary>
+   public partial class TimerWindow : Window
+   {
+      const string SERVICE_KEY = "2c69bdbf75324227bbb213129182906";
+      const string SERVICE_DOMAIN = "https://api.worldweatheronline.com/premium/v1/";
+      const string TIMEZONE_SERVICE_PATH = "tz.ashx";
+      const string WEATHER_SERVICE_PATH = "weather.ashx";
+      const string WEATHER_ADDITIONAL_ARGUMENTS = "&date=today&fx=no&cc=yes&show_comments=no&num_of_days=1&mca=no";
+
+      public string URI_IMAGE_SOURCE { get; set; }
+
+      private DispatcherTimer timer;
+      private int offset;
+
+      public TimerWindow()
+      {
+         InitializeComponent();
+
+         LocationInput locationWindow = new LocationInput();
+         locationWindow.ShowDialog();
+
+         var timeZoneResponse = FetchLocationInformation(LocationInput.City, LocationInput.Country, TIMEZONE_SERVICE_PATH);
+         SetTimeZoneFromResponse(timeZoneResponse);
+
+         var localWeatherResponse = FetchLocationInformation(LocationInput.City, LocationInput.Country, WEATHER_SERVICE_PATH, WEATHER_ADDITIONAL_ARGUMENTS);
+         SetWeatherIcon(localWeatherResponse);
+
+         SetTime();
+
+         timer = new DispatcherTimer(
+            GetClockUpdateInterval(),
+            DispatcherPriority.Normal,
+            delegate { TimerElapsed(); },
+            Dispatcher);
+      }
+
+      private HttpWebResponse FetchLocationInformation(string city, string country, string service, string additionalArguments = "")
+      {
+         var location = (city + ',' + country).Replace(" ", "+");
+         var url = SERVICE_DOMAIN + service + $"?q={location}&key={SERVICE_KEY}" + additionalArguments;
+         var request = (HttpWebRequest)WebRequest.Create(url);
+         return (HttpWebResponse)request.GetResponse();
+      }
+
+      public struct TimeZone
+      {
+         public string UTCOffset { get; set; }
+      }
+
+      private void SetTimeZoneFromResponse(HttpWebResponse timeZoneResponse)
+      {
+         using (var xmlReader = new StreamReader(timeZoneResponse.GetResponseStream()))
+         {
+            var doc = XDocument.Load(xmlReader);
+            var value = (from locationInfo in doc.Descendants("time_zone")
+               select new TimeZone
+               {
+                  UTCOffset = locationInfo.Element("utcOffset").Value
+               }).First().UTCOffset;
+
+            offset = Int32.Parse(value, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign);
+         }
+      }
+
+      public struct Weather
+      {
+         public string IconUrl { get; set; }
+      }
+
+      private void SetWeatherIcon(HttpWebResponse localWeatherResponse)
+      {
+         using (var xmlReader = new StreamReader(localWeatherResponse.GetResponseStream()))
+         {
+            var doc = XDocument.Load(xmlReader);
+            var value = (from locationInfo in doc.Descendants("current_condition")
+               select new Weather
+               {
+                  IconUrl = locationInfo.Element("weatherIconUrl").Value
+               }).First().IconUrl;
+
+            URI_IMAGE_SOURCE = value;
+            WeatherIcon.GetBindingExpression(Image.SourceProperty).UpdateTarget();
+         }
+      }
+
+      private void SetTime()
+      {
+         dateText.Text = DateTime.UtcNow.AddHours(offset).ToString("h:mm tt");
+      }
+
+      private void TimerElapsed()
+      {
+         timer.Interval = GetClockUpdateInterval();
+         timer.Start();
+         SetTime();
+      }
+
+      private TimeSpan GetClockUpdateInterval()
+      {
+         const int SECONDS_IN_MINUTE = 60;
+         const int MILLISECOND_IN_SECOND = 1000;
+
+         // To ensure the update happens after the minute updates, not right before.
+         const int UPDATE_DELAY_WINDOW = 15;
+
+         var now = DateTime.Now;
+         var millisecondsUntilUpdate =
+            (SECONDS_IN_MINUTE - now.Second)
+            * MILLISECOND_IN_SECOND - now.Millisecond
+            + UPDATE_DELAY_WINDOW;
+
+         return TimeSpan.FromMilliseconds(millisecondsUntilUpdate);
+      }
+   }
+}
